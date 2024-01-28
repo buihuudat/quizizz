10f6a922cf13590f34497393c11369519d0b76bf
Bui Huu Dat <dat54261001@gmail.com>
2024-01-28 22:54:11 +0700

    push

---
+++ scripts/auth.js
@@ -0,0 +1,26 @@
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
+
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
+});
+
+const togglePasswordVisibility = () => {
+  const passwordInput = document.getElementById("password");
+  const eyeIcon = document.querySelector(".eye-icon");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  }
+};
